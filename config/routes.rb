3b8ef08e07c2f341946f1266638fa0719da2702a
imThePhantom <phanductoan2005@outlook.com>
2015-07-03 13:22:56 +0700

    f user done

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  resources :users
 
   root "demo_pages#home"
   get "help" => "demo_pages#help"
   get "signup" => "users#new"
+  get "login" => 'sessions#new'
+  post "login" => "sessions#create"
+  delete "logout" => "sessions#destroy"
 
-  resources :users
 end
